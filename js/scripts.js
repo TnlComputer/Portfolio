28af971d4373afc0585d9e0b640328bdea7c3233
Jorge Martinez <tnlcomputer@gmail.com>
2025-08-28 19:50:38 -0300

    Actualizo portafolio github

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,28 @@
-const toggleTheme = document.getElementById("toggle-theme");
-const toggleIcon = document.getElementById("toggle-icon");
-const toggleText = document.getElementById("toggle-text");
-const toggleColor = document.getElementById("toggle-colors");
+const toggleTheme = document.getElementById('toggle-theme');
+const toggleIcon = document.getElementById('toggle-icon');
+const toggleText = document.getElementById('toggle-text');
+const toggleColor = document.getElementById('toggle-colors');
 
-const buttons = document.getElementById("buttons");
-const textsToChange = document.querySelectorAll("[data-section]");
-const footerCopy = document.getElementById("footer-copy");
+const buttons = document.getElementById('buttons');
+const textsToChange = document.querySelectorAll('[data-section]');
+const footerCopy = document.getElementById('footer-copy');
 
 const rootStyles = document.documentElement.style;
 
-toggleTheme.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-  if (toggleIcon.classList.contains("fa-moon")) {
-    toggleIcon.classList.replace("fa-moon", "fa-sun");
-    toggleText.textContent = "Light Mode";
+toggleTheme.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  if (toggleIcon.classList.contains('fa-moon')) {
+    toggleIcon.classList.replace('fa-moon', 'fa-sun');
+    toggleText.textContent = 'Light Mode';
   } else {
-    toggleIcon.classList.replace("fa-sun", "fa-moon");
-    toggleText.textContent = "Dark Mode";
+    toggleIcon.classList.replace('fa-sun', 'fa-moon');
+    toggleText.textContent = 'Dark Mode';
   }
 });
 
-toggleColor.addEventListener("click", (e) => {
-  if (e.target.classList.contains("colors__item")) {
-    rootStyles.setProperty("--primary-color", e.target.dataset.color);
+toggleColor.addEventListener('click', (e) => {
+  if (e.target.classList.contains('colors__item')) {
+    rootStyles.setProperty('--primary-color', e.target.dataset.color);
   }
 });
 
@@ -44,6 +44,6 @@ const changeLanguage = async (language) => {
 //   }
 // });
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
   footerCopy.textContent = `TnlComputer ${new Date().getFullYear()}`;
 });
